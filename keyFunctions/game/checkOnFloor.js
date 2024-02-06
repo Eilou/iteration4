62e52fc15b307812e1ce32f5a797ec4c11aa4dcd
Eilou <louiewbrooks04@gmail.com>
2024-02-06 14:37:42 +0000

    testing

--- keyFunctions/game/checkOnFloor.js
+++
@@ -1,7 +0,0 @@
-//THIS CODE WAS WRITTEN BY LOUIE BROOKS (as of 09/09/22)
-//IT IS CHECKING IF THE PLAYER IS ON THE FLOOR
-function checkOnFloor(player, walls) {
-    if (!colliding(player,walls)) {
-        player.onFloor = false;
-    }
-}
